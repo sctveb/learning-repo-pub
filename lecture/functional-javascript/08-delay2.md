0b343a9208d702f5221c91fa628a078e06e0fb6d
sctveb <sctveb@gmail.com>
2019-09-24 20:58:25 +0900

    2019-09-24-02 functional-javascript chp08 ended

--- lecture/functional-javascript/08-delay2.md
+++ lecture/functional-javascript/08-delay2.md
@@ -23,10 +23,159 @@ console.log(queryStr({ limit: 10, offset: 10, type: 'notice'}));
 
 
 ### 3. Array.prototype.join 보다 다형성이 높은 join 함수
+
+```javascript
+L.entries = function *(obj) {
+    for (const k in obj) yield [k, obj[k]];
+}
+
+const join = curry((sep = ',', iter) =>
+                  reduce((a,b) => `${a}${sep}${b}`, iter));
+
+const queryStr = pipe(
+    L.entries,
+    L.map(([k,v]) => `${k}=${v}`),
+    join('&')
+);
+function *a() {
+    yield 10;
+    yield 11;
+    yield 12;
+    yield 13;
+}
+a.join() // 에러
+console.log(join(' - ', a()));
+```
+
+
+
 ### 4. take, find
+```javascript
+const users = [
+    { age: 32 },
+    { age: 31 },
+    { age: 37 },
+    { age: 28 },
+    { age: 25 },
+    { age: 32 },
+    { age: 31 },
+    { age: 37 }
+]
+
+const find = (f, iter) => go(
+    iter,
+    L.filter(f),
+    take(1),
+    ([a]) => a
+);
+
+console.log(find(u => u.age < 30, users));
+
+go(users,
+  L.map(u => u.age),
+  find(n => n  < 30),
+  console.log);
+```
+
+
+
 ### 5. L.map, L.filter로 map과 filter 만들기
+
+```javascript
+const map = curry(pipe(
+  L.map,
+  take(Infinity)
+));
+
+const map = curry(pipe(
+  L.filter,
+  take(Infinity)
+));
+```
+
+
+
 ### 6. L.flatten, flatten
+
+```javascript
+const isIterable = a => a && a[Symbol.iterator];
+
+L.flatten = function *(iter) {
+  for (const a of iter) {
+    if (isIterable(a)) {
+      for (const b of a) {
+        yield b;
+      } 
+    } else {
+      yield a;
+    }
+  }
+};
+var it = L.flatten([1,2],3,4,[5,6],[7,8,9]);
+console.log([it]);
+const flatten = pipe(L.flatten, take(Infinity));
+console.log(flatten([1,2],3,4,[5,6],[7,8,9]));
+
+// yield *iterable === for (const val of iterable) yield val
+
+L.flatten = function *(iter) {
+  for (const a of iter) {
+    if (isIterable(a)) {
+      yield *a
+      } 
+    } else {
+      yield a;
+    }
+  }
+};
+
+// 깊은 iterable을 펼치기 위한 deepflat 메소드
+L.deepflat = function *f(iter) {
+    for (const a of iter) {
+        if (isIterable(a)) {
+            yield *f(a)
+        } else {
+            yield a;
+        }
+    }
+}
+```
+
+
+
 ### 7. L.flatMap, flatMap
+
+```javascript
+L.flatMap = curry(pipe(L.map, L.flatten));
+
+var it = L.flatMap(a => a, [[1,2], 3, [5,6,7]]);
+console.log([it]);
+const flatMap = pipe(L.flatMap, flatten);
+console.log(flatMap(range, [1,2,3]));
+```
+
+
+
 ### 8. 2차원 배열 다루기
+
+```javascript
+const arr = [
+    [1,2],
+    [3,4,5],
+    [6,7,8],
+    [9,10]
+];
+
+go(arr,
+    L.flatten,
+    L.filter(a => a % 2),
+    take(3),
+    console.log);
+```
+
+
+
 ### 9. 지연성 / 이터러블 중심 프로그래밍 실무적인 코드
+
+데이터를 우선적으로 정리 후 프로그래밍 메소드 작성을 하는 객체 지향 프로그래밍과 달리 함수 조합에 맞는 데이터를 구성
 
--- lecture/functional-javascript/cheatboard.js
+++ lecture/functional-javascript/cheatboard.js
@@ -1,62 +1,12 @@
 const curry = f => (a, ..._) => (_.length ? f(a, ..._) : (..._) => f(a, ..._));
 
-const map = curry((f, iter) => {
-  let res = [];
-  iter = iter[Symbol.iterator]();
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    res.push(f(a));
-  }
-  return res;
-});
-
-const filter = curry((f, iter) => {
-  let res = [];
-  for (const a of iter) {
-    if (f(a)) res.push(a);
-  }
-  return res;
-});
-
-const reduce = curry((f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  } else {
-    iter = iter[Symbol.iterator]();
-  }  
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    acc = f(acc, a);
-  }
-  return acc;
-});
-
 const go = (...args) => {
   return reduce((a, f) => f(a), args);
 };
 
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 
-function test(name, time, f) {
-  console.time(name);
-  while (time--) f();
-  console.timeEnd(name);
-}
-
-const take = (l, iter) => {
-  let res = [];
-  iter = iter[Symbol.iterator]();
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    res.push(a);
-    if (res.length == l) return res;
-  }
-  return res;
-};
+const isIterable = a => a && a[Symbol.iterator];
 
 const L = {};
 
@@ -85,8 +35,117 @@ L.filter = function*(f, iter) {
   }
 };
 
-const queryStr = obj => pipe(
-    Object.entries,
-    map(([k, v]) => `${k}=${v}`),
-    reduce((a,b) => `${a}&${b}`)
+L.filter = function*(f, iter) {
+  for (const a of iter) {
+    if (f(a)) yield a;
+  }
+};
+
+L.entries = function *(obj) {
+  for (const k in obj) yield [k, obj[k]];
+};
+
+L.flatten = function *(iter) {
+  for (const a of iter) {
+    if (isIterable(a)) {
+      for (const b of a) {
+        yield b;
+      } 
+    } else {
+      yield a;
+    }
+  }
+};
+
+L.deepflat = function *f(iter) {
+  for (const a of iter) {
+      if (isIterable(a)) {
+          yield *f(a)
+      } else {
+          yield a;
+      }
+  }
+};
+
+L.flatMap = curry(pipe(L.map, L.flatten));
+
+// const map = curry((f, iter) => {
+//   let res = [];
+//   iter = iter[Symbol.iterator]();
+//   let cur;
+//   while (!(cur = iter.next()).done) {
+//     const a = cur.value;
+//     res.push(f(a));
+//   }
+//   return res;
+// });
+
+const map = curry(pipe(
+  L.map,
+  take(Infinity)
+));
+
+// const filter = curry((f, iter) => {
+//   let res = [];
+//   for (const a of iter) {
+//     if (f(a)) res.push(a);
+//   }
+//   return res;
+// });
+
+const filter = curry(pipe(
+  L.filter,
+  take(Infinity)
+));
+
+const reduce = curry((f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  } else {
+    iter = iter[Symbol.iterator]();
+  }  
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    acc = f(acc, a);
+  }
+  return acc;
+});
+
+function test(name, time, f) {
+  console.time(name);
+  while (time--) f();
+  console.timeEnd(name);
+}
+
+const take = (l, iter) => {
+  let res = [];
+  iter = iter[Symbol.iterator]();
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    res.push(a);
+    if (res.length == l) return res;
+  }
+  return res;
+};
+
+const flatten = pipe(L.flatten, take(Infinity));
+
+const flatMap = pipe(L.flatMap, flatten);
+
+// const queryStr = obj => pipe(
+//     Object.entries,
+//     map(([k, v]) => `${k}=${v}`),
+//     reduce((a,b) => `${a}&${b}`)
+// );
+
+const join = curry((sep = ',', iter) =>
+                reduce((a,b) => `${a}${sep}${b}`, iter));
+
+const queryStr = pipe(
+  L.entries,
+  L.map(([k,v]) => `${k}=${v}`),
+  join('&')
 );
