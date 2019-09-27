0fb971f699eaa8c6b821e20e6c7fc84efd1e4e5c
sctveb <sctveb@gmail.com>
2019-09-27 15:21:31 +0900

    2019-09-27-01 functional js done until 10-2

--- lecture/functional-javascript/09-async1.md
+++ lecture/functional-javascript/09-async1.md
@@ -56,16 +56,91 @@ go1(go1(n2, add5), console.log);
 
 ### 4. 합성 관점에서의 Promise와 모나드
 
+모나드 : 안전한 함수 합성을 위한 도구
 
+```javascript
+const g = a => a + 1;
+const f = a => a * a;
+
+console.log(f(g(1)));
+console.log(f(g()));
+// 값의 유무와 상관없이 안전한 함수의 합성이 가능할까?
+[1].map(g).map(f).forEach(r => console.log(r));
+[].map(g).map(f).forEach(r => console.log(r));
+// 제일 앞단에서 값의 유무에 따라 연산을 하는 여부가 달라짐.
+[1, 2, 3].map(g).filter(a => a % 2).map(f).forEach(r => console.log(r));
+Array.of(1).map(g).map(f).forEach(r => console.log(r));
+Promise.resolve(1).then(g).then(f).then(r => console.log(r));
+
+new Promise(resolve =>
+            setTimeout(() => resolve(2), 100)
+           ).then(g).then(f).then(r => console.log(r));
+```
 
 
 
 ### 5. Kleisli Composition 관점에서의 Promise
 
+Kleisli Composition : 오류가 있을 수 있는 상황에서 함수 합성을 안전하게 하는 규칙
+
+오류가 발생할 경우 다음 합성이 이뤄지지 않도록 간주
+
+```javascript
+var users = [
+    { id: 1, name: 'aa'},
+    { id: 2, name: 'bb'},
+    { id: 3, name: 'cc'},
+];
+
+const getUserById = id => find(u => u.id == id, users);
+
+const f = ({name}) => name;
+const g = getUserById;
+
+// 변경 전
+const fg = id => f(g(id));
+
+const r = fg(2);
+console.log(r);
+
+users.pop();
+users.pop();
+
+const r2 = fg(2); // Error
+console.log(r2);
+
+// 변경 후
+const getUserById2 = id => find(u => u.id == id, users) || Promise.reject('d없음');
+
+const fg2 = id => Promise.resolve(id).then(g).then(f).catch(a => a);
+
+fg2(1).then(console.log);
+
+fg2(2).then(console.log);
+```
+
 
 
 ### 6. go, pipe, reduce에서 비동기 제어
 
+```javascript
+go(Promise.resolve(1),
+   a => a + 10,
+   a => Promise.reject('error~~'),
+   a => console.log('----'),
+   a => a + 1000,
+   console.log).catch(a => console.log(a));
+```
 
 
-### 7. promise.then의 중요한 규칙
+
+### 7. Promise.then의 중요한 규칙
+
+아무리 Promise를 중첩시켜도 `Promise.then`을 수행하면 Promise가 아닌 값이 나온다.
+
+```javascript
+Promise.resolve(Promise.resolve(Promise.resolve(1))).then(console.log);
+
+new Promise(resolve => resolve(new Promise(resolve => resolve(1)))).then(console.log);
+```
+
--- lecture/functional-javascript/cheatboard.js
+++ lecture/functional-javascript/cheatboard.js
@@ -1,12 +1,33 @@
+const nop = Symbol('nop');
+
 const curry = f => (a, ..._) => (_.length ? f(a, ..._) : (..._) => f(a, ..._));
 
 const go = (...args) => {
   return reduce((a, f) => f(a), args);
 };
 
+const go1 = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
+
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 
 const isIterable = a => a && a[Symbol.iterator];
+
+const take = curry((l, iter) => {
+  let res = [];
+  iter = iter[Symbol.iterator]();
+  return function recur() {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      if (a instanceof Promise)
+        return a.then(
+          a => (res.push(a), res).length == l ? res : recur()
+          ).catch(e => e == nop ? recur() : Promise.reject(e));
+      res.push(a);
+      if (res.length == l) return res;
+    }
+  };
+});
 
 const L = {};
 
@@ -22,16 +43,16 @@ L.map = function*(f, iter) {
   let cur;
   while (!(cur = iter.next()).done) {
     const a = cur.value;
-    yield f(a);
+    yield g1(a, f);
   }
 };
 
 L.filter = function*(f, iter) {
-  iter = iter[Symbol.iterator]();
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    if (f(a)) yield a;
+  for (const a of iter) {
+    const b = go1(a, f);
+    if (b instanceof Promise) {
+      yield b.then(b => b ? a : Promise.reject(nop))
+    } else if (b) yield a;
   }
 };
 
@@ -41,33 +62,38 @@ L.filter = function*(f, iter) {
   }
 };
 
-L.entries = function *(obj) {
+L.entries = function*(obj) {
   for (const k in obj) yield [k, obj[k]];
 };
 
-L.flatten = function *(iter) {
+L.flatten = function*(iter) {
   for (const a of iter) {
     if (isIterable(a)) {
       for (const b of a) {
         yield b;
-      } 
+      }
     } else {
       yield a;
     }
   }
 };
 
-L.deepflat = function *f(iter) {
+L.deepflat = function* f(iter) {
   for (const a of iter) {
-      if (isIterable(a)) {
-          yield *f(a)
-      } else {
-          yield a;
-      }
+    if (isIterable(a)) {
+      yield* f(a);
+    } else {
+      yield a;
+    }
   }
 };
 
-L.flatMap = curry(pipe(L.map, L.flatten));
+L.flatMap = curry(
+  pipe(
+    L.map,
+    L.flatten
+  )
+);
 
 // const map = curry((f, iter) => {
 //   let res = [];
@@ -80,10 +106,12 @@ L.flatMap = curry(pipe(L.map, L.flatten));
 //   return res;
 // });
 
-const map = curry(pipe(
-  L.map,
-  take(Infinity)
-));
+const map = curry(
+  pipe(
+    L.map,
+    take(Infinity)
+  )
+);
 
 // const filter = curry((f, iter) => {
 //   let res = [];
@@ -93,24 +121,46 @@ const map = curry(pipe(
 //   return res;
 // });
 
-const filter = curry(pipe(
-  L.filter,
-  take(Infinity)
-));
+const filter = curry(
+  pipe(
+    L.filter,
+    take(Infinity)
+  )
+);
 
+// const reduce = curry((f, acc, iter) => {
+//   if (!iter) {
+//     iter = acc[Symbol.iterator]();
+//     acc = iter.next().value;
+//   } else {
+//     iter = iter[Symbol.iterator]();
+//   }
+//   let cur;
+//   while (!(cur = iter.next()).done) {
+//     const a = cur.value;
+//     acc = f(acc, a);
+//   }
+//   return acc;
+// });
+
+// Promise에도 작동하는 reduce
 const reduce = curry((f, acc, iter) => {
   if (!iter) {
     iter = acc[Symbol.iterator]();
     acc = iter.next().value;
   } else {
     iter = iter[Symbol.iterator]();
-  }  
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    acc = f(acc, a);
   }
-  return acc;
+  return go1(acc, function recur(acc) {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      acc = f(acc, a);
+      if (acc instanceof Promise) return acc.then(recur);
+      // acc = acc instanceof Promise ? acc.then(acc => f(acc, a)) : f(acc, a);
+    }
+    return acc;
+  });
 });
 
 function test(name, time, f) {
@@ -119,21 +169,15 @@ function test(name, time, f) {
   console.timeEnd(name);
 }
 
-const take = (l, iter) => {
-  let res = [];
-  iter = iter[Symbol.iterator]();
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    res.push(a);
-    if (res.length == l) return res;
-  }
-  return res;
-};
+const flatten = pipe(
+  L.flatten,
+  take(Infinity)
+);
 
-const flatten = pipe(L.flatten, take(Infinity));
-
-const flatMap = pipe(L.flatMap, flatten);
+const flatMap = pipe(
+  L.flatMap,
+  flatten
+);
 
 // const queryStr = obj => pipe(
 //     Object.entries,
@@ -141,11 +185,12 @@ const flatMap = pipe(L.flatMap, flatten);
 //     reduce((a,b) => `${a}&${b}`)
 // );
 
-const join = curry((sep = ',', iter) =>
-                reduce((a,b) => `${a}${sep}${b}`, iter));
+const join = curry((sep = ",", iter) =>
+  reduce((a, b) => `${a}${sep}${b}`, iter)
+);
 
 const queryStr = pipe(
   L.entries,
-  L.map(([k,v]) => `${k}=${v}`),
-  join('&')
+  L.map(([k, v]) => `${k}=${v}`),
+  join("&")
 );
